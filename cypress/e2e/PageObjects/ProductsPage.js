cc1d2f5165d34c74ff1b72306a5ee91a4ba795c4
marianososa <nano.sosa92@gmail.com>
2022-10-17 18:15:57 -0300

    Test Case 8: Verify All Products and product detail page
    Adding test scripts and page object to verify the functionality of details page

--- cypress/e2e/PageObjects/ProductsPage.js
+++ cypress/e2e/PageObjects/ProductsPage.js
@@ -1,5 +1,7 @@
 const productSearchTxt = '[id="search_product"]';
 const submitSearchBtn = '[id="submit_search"]';
+const productDetailsLst = '.features_items .nav > li >';
+const productInformation = '.product-information';
 
 class productsPage {
   static searchForProduct(product) {
@@ -9,6 +11,19 @@ class productsPage {
 
   static verifySearchResults(resultItem) {
     cy.contains(resultItem).should('be.visible');
+  }
+
+  static clickViewProductOfFirstItem() {
+    cy.get(productDetailsLst).eq(0).click();
+  }
+
+  static verifyProductDetails(name, category, price, availability, condition, brand) {
+    cy.get(productInformation).contains(name);
+    cy.get(productInformation).contains(category);
+    cy.get(productInformation).contains(price);
+    cy.get(productInformation).contains(availability);
+    cy.get(productInformation).contains(condition);
+    cy.get(productInformation).contains(brand);
   }
 }
 
