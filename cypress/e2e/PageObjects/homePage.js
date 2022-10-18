36f7cd17fec9d595e8ae462e1eb6d7bb05e6d504
marianososa <nano.sosa92@gmail.com>
2022-10-18 16:34:52 -0300

    Test cases 10 and 11: Verify subscription in home and cart pages
    Adding test scripts and updating functionalities in the homepage page object to cover the subscription functionality

--- cypress/e2e/PageObjects/homePage.js
+++ cypress/e2e/PageObjects/homePage.js
@@ -1,8 +1,16 @@
+import { faker } from '@faker-js/faker';
+
 const logOutLnk = '.shop-menu > .nav > :nth-child(4) > a';
 const logoImg = '.logo';
 const testCasesLnk = '.shop-menu > .nav > :nth-child(5) > a';
 const productsLnk = 'Products';
 const allProductsLbl = 'all products';
+const subscriptionLbl = 'SUBSCRIPTION';
+const subscribeTxt = '[id="susbscribe_email"]';
+const arrowIconBtn = '[class="fa fa-arrow-circle-o-right"]';
+const successfullSubscribedLbl = '.alert-success';
+const cartLnk = 'Cart';
+const shoppingCartLbl = '.active';
 
 class homePage {
   static clickLogOut() {
@@ -17,6 +25,11 @@ class homePage {
     cy.get(logoImg).should('be.visible');
   }
 
+  static navigateToCartpage() {
+    cy.contains(cartLnk).click();
+    cy.get(shoppingCartLbl).should('be.visible');
+  }
+
   static clickOnTestCases() {
     cy.get(testCasesLnk).click();
   }
@@ -24,6 +37,19 @@ class homePage {
   static navigateToProducts() {
     cy.contains(productsLnk).click();
     cy.contains(allProductsLbl, { matchCase: false }).should('be.visible');
+  }
+
+  static enterEmailInSubscription() {
+    cy.contains(subscriptionLbl, { matchCase: false }).should('be.visible');
+    cy.get(subscribeTxt).type(faker.internet.email());
+  }
+
+  static clickSubscribeArrowIcon() {
+    cy.get(arrowIconBtn).click();
+  }
+
+  static verifySubscribed() {
+    cy.get(successfullSubscribedLbl).should('be.visible');
   }
 }
 
